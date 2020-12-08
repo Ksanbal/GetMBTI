6b0172ce4215bbcec415a60530afca4670a310d8
ksanbal <dev.ksanbal@gmail.com>
2020-12-08 16:13:33 +0900

    optimized code

--- GetMBTIAlgorithm.py
+++ GetMBTIAlgorithm.py
@@ -1,36 +1,19 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 from itertools import product
 import csv
-# from tqdm import tqdm
 from CatHand import CatHandBot
+import traceback
 
 opts = webdriver.FirefoxOptions()
-
-# options = webdriver.ChromeOptions()
-# options.add_argument('headless')
-# options.add_argument('window-size=1920x1080')
-# options.add_argument("disable-gpu")
-opts.add_argument("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-
-# bot = webdriver.Chrome('./chromedriver', chrome_options=options)
-# bot = webdriver.Chrome('./chromedriver')
-
-# opts = webdriver.FirefoxOptions()
+opts.add_argument('Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:83.0) Gecko/20100101 Firefox/83.0')
 opts.add_argument("--headless")
 bot = webdriver.Firefox(executable_path='./geckodriver', options=opts)
 
 # 중복순열 4096개
 test = list(product([1,2], repeat=12))
-#test = [
-#    (2,2,2,2,2,2,2,2,2,2,2,2),
-#    (2,1,2,1,2,1,2,1,2,1,2,1)
-#]
-percent = 0
+result_img = []
 
 with open('result.csv', 'w') as f:
 	pass
@@ -42,37 +25,27 @@ try:
 
         bot.find_element_by_name('dogNm').send_keys('Hello :)')
         bot.find_element_by_class_name('js_start_btn').click()
-        # time.sleep(1)
 
         for t in test_list:
             time.sleep(1.5)
             if t == 1:
-                # WebDriverWait(bot, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'answer_box.answer_a')))
                 bot.find_element_by_class_name('answer_box.answer_a').click()
             else:
-                # WebDriverWait(bot, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'answer_box.answer_b')))
                 bot.find_element_by_class_name('answer_box.answer_b').click()
-        
-        # WebDriverWait(bot, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'img_illustration')))
+
         time.sleep(1)
         result = bot.find_element_by_class_name('img_illustration').get_attribute('src')
         result = re.compile('\w{4}.png').findall(result)[0][:4]
 
-        bot.save_screenshot(f'img/{result}.png')
-
-        test_list.append(result)
+        if result not in result_img:
+            bot.save_screenshot(f'img/{result}.png')
+            result_img.append(result)
 
         with open('result.csv', 'a') as f:
-            wr = csv.writer(f)
-            wr.writerow(test_list)
+            csv.writer(f).writerow(test_list+[result])
 
-        percent += 1
-
-        if percent % 100 == 0:
-            CatHandBot.sendTalk(f'열심히 하는중 : {percent}%')    
     CatHandBot.sendTalk('끝났엄')
     bot.quit()
     
 except Exception as ex:
-    CatHandBot.sendTalk(f'에러 났따아...\n*****\nError{ex}\n*****')
-    print('*****\nError', ex, '\n*****')
+    CatHandBot.sendTalk(f'에러 났따아...\n*****\nError\n{traceback.format_exc()}\n*****')
