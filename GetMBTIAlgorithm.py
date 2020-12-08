66fe869b1046dc07316ecb229f43bfb852fb221d
ksanbal <dev.ksanbal@gmail.com>
2020-12-08 15:46:31 +0900

    fix code for firefox

--- GetMBTIAlgorithm.py
+++ GetMBTIAlgorithm.py
@@ -1,38 +1,47 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 from itertools import product
 import csv
-from tqdm import tqdm
+# from tqdm import tqdm
 from CatHand import CatHandBot
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-options.add_argument("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+opts = webdriver.FirefoxOptions()
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument("disable-gpu")
+opts.add_argument("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 
 # bot = webdriver.Chrome('./chromedriver', chrome_options=options)
-bot = webdriver.Chrome('./chromedriver')
+# bot = webdriver.Chrome('./chromedriver')
+
+# opts = webdriver.FirefoxOptions()
+opts.add_argument("--headless")
+bot = webdriver.Firefox(executable_path='./geckodriver', options=opts)
 
 # 중복순열 4096개
-# test = list(product([1,2], repeat=12))
-test = [
-    (2,2,2,2,2,2,2,2,2,2,2,2),
-    (2,1,2,1,2,1,2,1,2,1,2,1)
-]
+test = list(product([1,2], repeat=12))
+#test = [
+#    (2,2,2,2,2,2,2,2,2,2,2,2),
+#    (2,1,2,1,2,1,2,1,2,1,2,1)
+#]
 percent = 0
 
+with open('result.csv', 'w') as f:
+	pass
+
 try:
-    for i in tqdm(test):
+    for i in test:
         test_list = list(i)
         bot.get('https://m.fitpetmall.com/main/html.php?htmid=service/mbti_test.html')
 
-        name_box = bot.find_element_by_name('dogNm').send_keys('Hello :)')
-        next_btn = bot.find_element_by_class_name('js_start_btn').click()
+        bot.find_element_by_name('dogNm').send_keys('Hello :)')
+        bot.find_element_by_class_name('js_start_btn').click()
         # time.sleep(1)
 
         for t in test_list:
@@ -59,7 +68,7 @@ try:
 
         percent += 1
 
-        if percent % 500 == 0:
+        if percent % 100 == 0:
             CatHandBot.sendTalk(f'열심히 하는중 : {percent}%')    
     CatHandBot.sendTalk('끝났엄')
     bot.quit()
